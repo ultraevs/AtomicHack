7717ab7f0b6f8e3e84a88a0e140251112fe818a4
ultraevs <evstabpro@gmail.com>
2024-06-05 15:31:59 +0300

    init

---
+++ go-backend/internal/api/router/router.go
@@ -0,0 +1,35 @@
+package router
+
+import (
+	"app/internal/api/middleware"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+type Router struct {
+	engine *gin.Engine
+}
+
+func NewRouter() Router {
+	return Router{engine: gin.Default()}
+}
+
+func (router *Router) Run(port string) error {
+	router.Setup()
+	return router.engine.Run(":" + port)
+}
+
+func (router *Router) Setup() {
+	gin.SetMode(gin.DebugMode)
+	router.engine.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://atomic.shmyaks.ru", "http://localhost:5173"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+	}))
+	router.engine.GET("v1/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	router.engine.Use(middleware.RateLimiterMiddleware())
+	//v1 := router.engine.Group("/v1")
+}
