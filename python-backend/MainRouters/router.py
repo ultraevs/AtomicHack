d24809cfa2c844f6589222282463e6e4fca9c2e5
ultraevs <evstabpro@gmail.com>
2024-06-11 18:52:03 +0300

    v1

--- python-backend/MainRouters/router.py
+++ python-backend/MainRouters/router.py
@@ -1,8 +1,10 @@
-from cv.nn import core
-from fastapi import FastAPI, APIRouter, HTTPException, UploadFile, File
-from ultralytics import YOLO
-import base64, cv2
+import base64
+import cv2
 import numpy as np
+from cv.nn import core
+from fastapi import FastAPI, APIRouter, HTTPException
+from pydantic import BaseModel
+from ultralytics import YOLO
 
 model = YOLO('cv/models/good-bad_v1.pt')
 
@@ -10,34 +12,45 @@ app = FastAPI()
 router = APIRouter(tags=["Model"])
 
 
+class ImageData(BaseModel):
+    image: str
+
+
 @router.post("/process-image")
-async def process_image(file: UploadFile = File(...)):
+async def process_image(data: ImageData):
     try:
-        image = base64_to_cv2(None) # сюда base64 с фронта добавь
+        image = base64_to_cv2(data.image)
 
         results = core.process(
             image=image,
             model=model
         )
 
-        # status: results -> result
-        # objects: results -> objects list [[class, confidence], ...]
-        # images: results -> images -> images in cv2! format
+        result1_path = './result1.jpg'
+        result2_path = './result2.jpg'
 
+        # преобразуем изображения в base64
+        result1_base64 = image_to_base64(result1_path)
+        result2_base64 = image_to_base64(result2_path)
 
-        # response
+        # формируем ответ
+        return {"result1": result1_base64, "result2": result2_base64}
 
     except Exception as e:
-        raise HTTPException(status_code=400, detail=e) from e
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 app.include_router(router)
 
+
 def base64_to_cv2(image_base64):
     image_data = base64.b64decode(image_base64)
-    
     np_array = np.frombuffer(image_data, np.uint8)
-    
     image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-    
     return image
+
+
+def image_to_base64(image_path):
+    with open(image_path, 'rb') as image_file:
+        image_base64 = base64.b64encode(image_file.read()).decode('utf-8')
+    return image_base64
--- python-backend/main.py
+++ python-backend/main.py
@@ -15,32 +15,32 @@ app.include_router(router)
 logging.basicConfig(filename='work.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-class RateLimitMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app, rate_limit=100, time_window=60):
-        super().__init__(app)
-        self.rate_limit = rate_limit
-        self.time_window = time_window
-        self.requests = {}
-
-    async def dispatch(self, request: Request, call_next):
-        client_ip = request.client.host
-        current_time = time.time()
-
-        requests = self.requests.get(client_ip, [])
-
-        # Очистка старых запросов
-        requests = [timestamp for timestamp in requests if current_time - timestamp < self.time_window]
-        self.requests[client_ip] = requests
-
-        if len(requests) >= self.rate_limit:
-            return Response(content="Too Many Requests", status_code=429)
-
-        self.requests[client_ip].append(current_time)
-        response = await call_next(request)
-        return response
-
-
-app.add_middleware(RateLimitMiddleware, rate_limit=5, time_window=60)
+# class RateLimitMiddleware(BaseHTTPMiddleware):
+#     def __init__(self, app, rate_limit=100, time_window=60):
+#         super().__init__(app)
+#         self.rate_limit = rate_limit
+#         self.time_window = time_window
+#         self.requests = {}
+#
+#     async def dispatch(self, request: Request, call_next):
+#         client_ip = request.client.host
+#         current_time = time.time()
+#
+#         requests = self.requests.get(client_ip, [])
+#
+#         # Очистка старых запросов
+#         requests = [timestamp for timestamp in requests if current_time - timestamp < self.time_window]
+#         self.requests[client_ip] = requests
+#
+#         if len(requests) >= self.rate_limit:
+#             return Response(content="Too Many Requests", status_code=429)
+#
+#         self.requests[client_ip].append(current_time)
+#         response = await call_next(request)
+#         return response
+#
+#
+# app.add_middleware(RateLimitMiddleware, rate_limit=5, time_window=60)
 
 app.add_middleware(
     CORSMiddleware,
