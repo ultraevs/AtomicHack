895855b2cf1c7f8cf549506ba5f91b5751cccab2
themeppci <themeppci@yandex.ru>
2024-06-12 16:49:09 +0300

    draw style 1 update, draw style 3 add

--- python-backend/cv/nn/core.py
+++ python-backend/cv/nn/core.py
@@ -18,7 +18,7 @@ def process(image64, model):
         for obj in result:
             return_.append([obj[0], obj[1]])
 
-        for draw_style in range(2):
+        for draw_style in range(3):
             cloned_image = image.copy()
             for obj in result:
                 cloned_image = draw(cloned_image, obj[2], draw_style, colorcode, obj[0], obj[1])
@@ -56,7 +56,7 @@ def draw(image, xyxyn, draw_style, colorcode, class_, conf):
     cv2.rectangle(image, (x1, y1), (x2, y2), colorcode[class_], 2)
     
     if draw_style == 0:
-        pass
+        add_legend(image, colorcode)
         
     elif draw_style == 1:
         text = f'{class_}: {conf}'
@@ -72,14 +72,47 @@ def draw(image, xyxyn, draw_style, colorcode, class_, conf):
         cv2.rectangle(image, (text_x-offset, text_y - text_size[1]-offset), (text_x+offset + text_size[0], text_y+offset), (32, 32, 32), cv2.FILLED)
 
         cv2.putText(image, text, (text_x, text_y), font, font_scale, color, thickness)
+    
+    elif draw_style == 2:
+        alpha = float(conf)
+        overlay = image.copy()
+        cv2.rectangle(overlay, (x1, y1), (x2, y2), colorcode[class_], -1)
+        cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0, image)
+        text = f'{class_}: {conf}'
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 1
+        color = (255, 255, 255)
+        thickness = 1
+        text_size, _ = cv2.getTextSize(text, font, font_scale, thickness)
+        text_x = x1
+        text_y = y1 - 10
+        offset = 5
 
+        cv2.rectangle(image, (text_x-offset, text_y - text_size[1]-offset), (text_x+offset + text_size[0], text_y+offset), (32, 32, 32), cv2.FILLED)
+
+        cv2.putText(image, text, (text_x, text_y), font, font_scale, color, thickness)
+    
     return image
 
+def add_legend(image, colorcode):
+    start_x = 10
+    start_y = 10
+    square_size = 20
+    padding = 5
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.5
+    color = (255, 255, 255)
+    thickness = 1
+
+    for idx, (class_name, class_color) in enumerate(colorcode.items()):
+        top_left = (start_x, start_y + idx * (square_size + padding))
+        bottom_right = (start_x + square_size, start_y + square_size + idx * (square_size + padding))
+        cv2.rectangle(image, top_left, bottom_right, class_color, -1)
+        cv2.putText(image, class_name, (start_x + square_size + padding, start_y + square_size - padding + idx * (square_size + padding)), font, font_scale, color, thickness)
 
 def image_to_base64(image):
     _, buffer = cv2.imencode('.jpg', image)
     image_base64 = base64.b64encode(buffer).decode('utf-8')
-    
     return image_base64
 
 def base64_to_cv2(image_base64):
@@ -87,3 +120,25 @@ def base64_to_cv2(image_base64):
     np_array = np.frombuffer(image_data, np.uint8)
     image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
     return image
+
+
+# Example usage
+if __name__ == "__main__":
+    model = YOLO('cv/models/good-bad_v1.pt')
+
+    image = cv2.imread('cv/testing/good.jpg')
+
+    image_base64 = image_to_base64(image)
+
+    results = process(
+        image64=image_base64,
+        model=model
+    )
+
+    print(results['result'], results['objects'])
+
+    c = 0
+    for image_base64 in results['images']:
+        c += 1
+        image = base64_to_cv2(image_base64)
+        cv2.imwrite(f'result{c}.jpg', image)
--- python-backend/cv/use.py
+++ python-backend/cv/use.py
@@ -1,25 +1,36 @@
 from ultralytics import YOLO
 from nn import core
 import cv2
+import base64
+import numpy as np
 
+def cv2_to_base64(image):
+    _, buffer = cv2.imencode('.jpg', image)
+    image_base64 = base64.b64encode(buffer).decode('utf-8')
+    return image_base64
 
+def base64_to_cv2(image_base64):
+    image_data = base64.b64decode(image_base64)
+    np_array = np.frombuffer(image_data, np.uint8)
+    image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+    return image
 
 model = YOLO('cv/models/good-bad_v1.pt')
 
 image = cv2.imread('cv/testing/good.jpg')
 
+image_base64 = cv2_to_base64(image)
+
 results = core.process(
-    image=image,
+    image64=image_base64,
     model=model
 )
 
-
-print(results['result'], results['objects'], results['images'])
+# Вывод результатов
+print(results['result'], results['objects'])
 
 c = 0
-for image in results['images']:
+for image_base64 in results['images']:
     c += 1
+    image = base64_to_cv2(image_base64)
     cv2.imwrite(f'result{c}.jpg', image)
-
-
-# from python-backend/
