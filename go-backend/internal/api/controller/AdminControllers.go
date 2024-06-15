325bb453a7c3037014172b6376ee5644c2476855
ultraevs <evstabpro@gmail.com>
2024-06-15 10:26:26 +0300

    fix responses

--- go-backend/internal/api/controller/AdminControllers.go
+++ go-backend/internal/api/controller/AdminControllers.go
@@ -49,7 +49,7 @@ func GetRecognitions(context *gin.Context) {
 	}
 
 	if len(recognitions) == 0 {
-		context.JSON(http.StatusOK, gin.H{})
+		context.JSON(http.StatusOK, []interface{}{})
 	} else {
 		context.JSON(http.StatusOK, recognitions)
 	}
